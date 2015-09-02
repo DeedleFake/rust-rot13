ba52f0701d76a5bbfe5777704540a5c27e3f9464
DeedleFake <deedlefake@users.noreply.github.com>
2015-09-02 09:41:34 -0400

    Switch to library.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-use std::io::{Read, BufReader, Result};
+use std::io::{Read, Result};
 
-struct Rot13<R: Read> {
+pub struct Rot13<R: Read> {
     r: R,
 }
 
 impl<R: Read> Rot13<R> {
-    fn new(r: R) -> Rot13<R> {
+    pub fn new(r: R) -> Rot13<R> {
         Rot13{
             r: r,
         }
@@ -20,7 +20,7 @@ impl<R: Read> Read for Rot13<R> {
                 return Err(err);
             }
         };
-        
+
         for i in 0..buf.len() {
             let c = buf[i] as char;
             if (c >= 'A') && (c <= 'Z') {
@@ -30,7 +30,7 @@ impl<R: Read> Read for Rot13<R> {
                 buf[i] = rotate(buf[i], 'z' as u8);
             }
         }
-        
+
         Ok(r)
     }
 }
@@ -40,14 +40,22 @@ fn rotate(mut c: u8, max: u8) -> u8 {
     if c > max {
         c -= 26;
     }
-    
+
     c
 }
 
-fn main() {
-    let mut r = Rot13::new(BufReader::new("Pna V unir fbzr pnxr, cyrnfr?".as_bytes()));
-    
-    let mut s = String::new();
-    r.read_to_string(&mut s).unwrap();
-    println!("{}", s);
+#[test]
+fn test_rot13() {
+    use std::io::{BufReader};
+
+    let tests = vec![
+        ("This is a test.".as_bytes(), "Guvf vf n grfg.".to_string()),
+    ];
+
+    for (t, ex) in tests {
+        let mut r = Rot13::new(BufReader::new(t));
+        let mut s = String::new();
+        r.read_to_string(&mut s).unwrap();
+        assert_eq!(s, ex);
+    }
 }
